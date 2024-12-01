6c3076fa4f6404bc5f1bf0a847a51a9cabc58fcf
jagdevsinghdosanjh@gmail.com <jagdevsinghdosanjh@gmail.com>
2024-12-01 22:44:04 +0530

    OK

--- Face_Recognition/dlib-19.24.6/dlib/java/build/CMakeFiles/cmake_directory_clean.cmake
+++
@@ -1,6 +0,0 @@
-file(REMOVE_RECURSE
-  "lib/java_src/net/dlib/swigJAVA_wrap.cxx"
-  "lib/java_src/net/dlib/global.java"
-  "lib/java_src/net/dlib/globalJNI.java"
-  "CMakeFiles/myproject.dir/swigJAVA.stamp"
-)
